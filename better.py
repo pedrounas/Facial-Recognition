c8523a15eb8987b1af66cfed35bcc950fe46d9ae
Pedro Unas <shockwave2k11@gmail.com>
2019-09-20 12:00:37 +0100

    Now parses a student list to find those in attendance

--- better.py
+++ better.py
@@ -1,4 +1,6 @@
-import cv2, os, sys
+import cv2
+import os
+import sys
 import numpy as np
 import face_recognition
 
@@ -9,6 +11,12 @@ files = os.listdir(path)
 names = []
 file_names = []
 encodings = []
+process_frame = True
+face_locations = []
+face_names = []
+face_encodings = []
+found_names = []
+students = []
 
 for file in files:
     x = file.split('.')
@@ -19,14 +27,19 @@ for file in file_names:
     temp = face_recognition.load_image_file(file)
     encodings.append(face_recognition.face_encodings(temp)[0])
 
-process_frame = True
-face_locations = []
-face_names = []
-face_encodings = []
+
+file = open('students.txt', 'r')
+for line in file:
+    students.append(line.rstrip('\n'))
+
+print(students)
+
 
 while True:
 
     ret, frame = vid.read()
+
+    crop = None
 
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
@@ -37,6 +50,11 @@ while True:
         face_encodings = face_recognition.face_encodings(
             rgb_small_frame, face_locations)
         face_names = []
+
+        # if len(face_locations) == 2:
+        #     temp = face_locations[0]
+        #     face_locations[0] = face_locations[1]
+        #     face_locations[1] = temp
 
         for face_encoding in face_encodings:
             matches = face_recognition.compare_faces(
@@ -59,16 +77,38 @@ while True:
         bottom *= 4
         left *= 4
 
-        cv2.rectangle(frame, (left, top), (right, bottom),
-                      (0, 0, 255), 1)
+        # print(name, top, bottom, left, right)
+        # crop = frame[top:bottom, left:right]
+
+        # cv2.rectangle(frame, (left, top), (right, bottom),
+        #               (0, 0, 255), 1)
+
         font = cv2.FONT_HERSHEY_DUPLEX
         name_ = name.replace("_", " ")
+        if name_ not in found_names and name_ != "Unknown":
+            found_names.append(name_)
+
         cv2.putText(frame, name_, (left + 6, bottom-6),
                     font, 1.0, (0, 0, 255), 1)
 
     cv2.imshow('Video', frame)
 
+    # cv2.imshow('Crop', crop)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        fp = open('attendance.txt', 'w')
+
+        fp.write('In class:\n')
+
+        for student in found_names:
+            if student in students:
+                fp.write(student + '\n')
+                students.remove(student)
+
+        fp.write('\nNot here:\n')
+
+        for student in students:
+            fp.write(student + '\n')
         break
 
 vid.release()
