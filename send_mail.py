365da00dbffbe653a1a98e3a5b341cc2493ee84d
Pedro Unas <pedrounas@MacBook-Pro-de-Pedro.local>
2019-11-27 00:51:56 +0000

    Convert file into function to be used by main program

--- send_mail.py
+++ send_mail.py
@@ -1,37 +1,40 @@
 import smtplib
 import ssl
-from datetime import  date
-
-gmail_user = 'missing.students.cv'
-# problema de segurança, pass do mail do sender em plaintext
-gmail_password = '52S[rheG'
-
-sent_from = 'missing.students.cv@gmail.com'
-# to = ['saraferreira.p3@gmail.com', 'pedrommunas@gmail.com']
-to = ['missing.students.cv@gmail.com'] # Para testar
-subject = 'Attendence for the class of ' + str(date.today().strftime("%d/%m/%Y"))
-body = None
-
-with open('./attendance.txt') as fp:
-    body = fp.read()
-
-message = """\
-From: %s
-To: %s
-Subject: %s
-\n%s
-""" % (sent_from, ", ".join(to), subject, body)
+from datetime import date
 
 
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login("missing.students.cv", "52S[rheG")
-    server.sendmail(
-        sent_from,
-        sent_from,
-        message)
-    server.quit()
-    print('Email sent!')
+def send():
 
-except:
-    print('Something went wrong...')
+    gmail_user = 'missing.students.cv'
+    # problema de segurança, pass do mail do sender em plaintext
+    gmail_password = '52S[rheG'
+
+    sent_from = 'missing.students.cv@gmail.com'
+    # to = ['saraferreira.p3@gmail.com', 'pedrommunas@gmail.com']
+    to = ['missing.students.cv@gmail.com']  # Para testar
+    subject = 'Attendence for the class of ' + \
+        str(date.today().strftime("%d/%m/%Y"))
+    body = None
+
+    with open('./attendance.txt') as fp:
+        body = fp.read()
+
+    message = """\
+    From: %s
+    To: %s
+    Subject: %s
+    \n\n%s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(gmail_user, gmail_password)
+        server.sendmail(
+            sent_from,
+            sent_from,
+            message)
+        server.quit()
+        print('Email sent!')
+
+    except:
+        print('Something went wrong...')
