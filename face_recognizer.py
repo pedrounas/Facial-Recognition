a62ed8aaf260946d99a5a80e320dc4d056c13395
saraferreirascf <saraferreira.p3@gmail.com>
2019-12-05 18:08:54 +0000

    Get the students name from sigarra

--- face_recognizer.py
+++ face_recognizer.py
@@ -5,15 +5,22 @@ import numpy as np
 import face_recognition
 import send_mail
 import get_names
+import datetime
+import scraper
+import time
 
-get_names.get_students()
+#get_names.get_students()
 
 # vid = cv2.VideoCapture('http://192.168.1.4:8080/video')
 vid = cv2.VideoCapture(0)
-
-path = './test/'
-files = os.listdir(path)
+now = datetime.datetime.now()
+week_day= datetime.datetime.now().weekday() #Return the day of the week as an integer, where Monday is 0 and Sunday is 6.
+print("Hora: %s/%s %s:%s" % (now.day, now.month, now.hour, now.minute))
+#while(week_day ==2 and now.hour>8 and now.minute>10 && now.hour<=10 && now.minute<=30): #se tá dentro do horario de aula
 names = []
+path = './known/'
+files = os.listdir(path)
+	
 file_names = []
 encodings = []
 process_frame = True
@@ -22,94 +29,108 @@ face_names = []
 face_encodings = []
 found_names = []
 students = []
+first_time=True;
+while(week_day ==3 and now.hour>14 and now.hour<18): #para testar 
+	now = datetime.datetime.now()
+	if(first_time==True): #so vai buscar o nome uma vez
+		print("****************************")
+		print("Aula de visão computacional")
+		print("****************************")
+		print()
+		print("Estudantes inscritos:")
+		print()
+		names=scraper.get_students_names()
+		
+	first_time=False;
+	#names.append(nome)
+	
+	#break
+	
 
-for file in files:
-    if file.endswith('.jpg'):
-        x = file.split('.')
-        file_names.append(path + file)
-        names.append(x[0])
+	for file in files:
+		if file.endswith('.jpg'):
+			x = file.split('.')
+			file_names.append(path + file)
+        	#names1.append(x[0])
 
-for file in file_names:
-    if file.endswith('.jpg'):
-        temp = face_recognition.load_image_file(file)
-        if face_recognition.face_encodings(temp):
-            encodings.append(face_recognition.face_encodings(temp)[0])
-            print(file)
+	for file in file_names:
+		if file.endswith('.jpg'):
+			temp = face_recognition.load_image_file(file)
+			if face_recognition.face_encodings(temp):
+				encodings.append(face_recognition.face_encodings(temp)[0])
+				#print(file)
 
 
-file = open('students.txt', 'r')
-for line in file:
-    students.append(line.rstrip('\n'))
+	file = open('students.txt', 'r')
+	for line in file:
+		students.append(line.rstrip('\n'))
 
-while True:
+	while True:
 
-    ret, frame = vid.read()
+		ret, frame = vid.read()
 
-    crop = None
+		crop = None
 
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+		small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    rgb_small_frame = small_frame[:, :, ::-1]
+		rgb_small_frame = small_frame[:, :, ::-1]
 
-    if process_frame:
-        face_names = []
-        face_locations = face_recognition.face_locations(
-            rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(
-            rgb_small_frame, known_face_locations=face_locations)
+		if process_frame:
+			face_names = []
+			face_locations = face_recognition.face_locations(
+            	rgb_small_frame)
+			face_encodings = face_recognition.face_encodings(
+            	rgb_small_frame, known_face_locations=face_locations)
 
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(
-                encodings, face_encoding)
+			for face_encoding in face_encodings:
+				matches = face_recognition.compare_faces(
+                	encodings, face_encoding)
 
-            name = "Unknown"
+				name = "Unknown"
 
-            face_distances = face_recognition.face_distance(
-                encodings, face_encoding)
+				face_distances = face_recognition.face_distance(
+                	encodings, face_encoding)
 
-            best_match = np.argmin(face_distances)
-            if matches[best_match]:
-                name = names[best_match]
-            face_names.append(name)
+				best_match = np.argmin(face_distances)
+				if matches[best_match]:
+					name = names[best_match]
+					face_names.append(name)
 
-    process_frame = not process_frame
+		process_frame = not process_frame
 
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+		for (top, right, bottom, left), name in zip(face_locations, face_names):
+			top *= 4
+			right *= 4
+			bottom *= 4
+			left *= 4
 
-        cv2.rectangle(frame, (left, top), (right, bottom),
-                      (0, 0, 255), 1)
+			cv2.rectangle(frame, (left, top), (right, bottom),
+                      	(0, 0, 255), 1)
 
-        font = cv2.FONT_HERSHEY_DUPLEX
-        name_ = name.replace("_", " ")
-        if name_ not in found_names and name_ != "Unknown":
-            found_names.append(name_)
+			font = cv2.FONT_HERSHEY_DUPLEX
+			name_ = name.replace("_", " ")
+			if name_ not in found_names and name_ != "Unknown":
+				found_names.append(name_)
 
-        cv2.putText(frame, name_, (left + 6, bottom-6),
-                    font, 1.0, (0, 0, 255), 1)
+				cv2.putText(frame, name_, (left + 6, bottom-6),
+                    	font, 1.0, (0, 0, 255), 1)
 
-    cv2.imshow('Video', frame)
+		cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        fp = open('attendance.txt', 'w+')
 
-        fp.write('In class:\n')
+fp = open('attendance.txt', 'w+')
+fp.write('In class:\n')
 
-        for student in found_names:
-            if student in students:
-                fp.write(student + '\n')
-                students.remove(student)
+for student in found_names:
+	if student in students:
+		fp.write(student + '\n')
+		students.remove(student)
 
-        fp.write('\nNot here:\n')
+		fp.write('\nNot here:\n')
 
-        for student in students:
-            fp.write(student + '\n')
-        fp.close()
-        send_mail.send()
-        break
-
+	for student in students:
+		fp.write(student + '\n')
+		fp.close()
+		send_mail.send()
 vid.release()
 cv2.destroyAllWindows()
--- scraper.py
+++ scraper.py
@@ -5,49 +5,49 @@ import urllib.request
 import os
 import requests
 
-user_name = "up201606726"
-password = "Chitaveloz98"
-driver = webdriver.Firefox()
-driver.get("https://sigarra.up.pt/fcup/pt/WEB_PAGE.INICIAL")
-element = driver.find_element_by_id("user")
-element.send_keys(user_name)
-element = driver.find_element_by_id("pass")
-element.send_keys(password)
-element.send_keys(Keys.RETURN)
-time.sleep(5) #para só avançar quando tiver feito mesmo o login 
+def get_students_names():
+	user_name = "up201606726"
+	password = "Chitaveloz98"
+	names=[]
+	driver = webdriver.Firefox()
+	driver.get("https://sigarra.up.pt/fcup/pt/WEB_PAGE.INICIAL")
+	element = driver.find_element_by_id("user")
+	element.send_keys(user_name)
+	element = driver.find_element_by_id("pass")
+	element.send_keys(password)
+	element.send_keys(Keys.RETURN)
+	time.sleep(1) #para só avançar quando tiver feito mesmo o login 
+	
+	#58 alunos (2-60)
+	for i in range (2,60):
+		if(i>=52): #mais de uma pagina de alunos
+			driver.get("https://sigarra.up.pt/fcup/pt/fest_geral.estudantes_inscritos_list?pv_num_pag=2&pv_ocorrencia_id=445367")
+			element=driver.find_element_by_xpath("//*[@id='conteudoinner']/table/tbody/tr["+str(i-50)+"]/td[1]/a")
+		else:
+			driver.get("https://sigarra.up.pt/fcup/pt/fest_geral.estudantes_inscritos_list?pv_ocorrencia_id=445367")
+			element=driver.find_element_by_xpath("//*[@id='conteudoinner']/table/tbody/tr["+str(i)+"]/td[1]/a")
+		element.click()
+		time.sleep(3)
+		name=driver.find_element_by_xpath("//*[@id='conteudoinner']/h1[2]")
 
+		if(name is None):
+			name=driver.find_element_by_xpath("/html/body/div[4]/div[3]/div[3]/div/div[2]/div/div[2]/div[1]")
+			if(name is None):
+				#Feup
+				name=driver.find_element_by_xpath("/html/body/div[4]/div[3]/div[3]/div/h1[2]")
+		print(name.text)
+		names.append(name.text)
+		#img_url = driver.find_element_by_xpath("/html/body/div[4]/div[3]/div[3]/div/div[2]/div/div[1]/div/img").get_attribute("src")
+		#o link da imagem esta certo
+		#print(img_url)
+		#img_name = str(i)+"jpg"
+		##Método1 - dá forbidden
 
-#50 alunos, posteriormente pedir como argumento ou usar o scraper para buscar numero de alunos
-for i in range (2,52):
-	driver.get("https://sigarra.up.pt/fcup/pt/fest_geral.estudantes_inscritos_list?pv_ocorrencia_id=445367")
-	element=driver.find_element_by_xpath("//*[@id='conteudoinner']/table/tbody/tr["+str(i)+"]/td[1]/a")
-	element.click()
-	time.sleep(5)
-	img_url = driver.find_element_by_xpath("/html/body/div[4]/div[3]/div[3]/div/div[2]/div/div[1]/div/img").get_attribute("src")
-	#o link da imagem esta certo
-	print(img_url)
-	img_name = str(i)+"jpg"
-	##Método1 - dá forbidden
-
-	#urlretrieve(image, "student"+str(i)+".jpg")
-	##Método2 - dá uma imagem sem cara
-		
-	#r = requests.get(img_url,
-                 #stream=True, headers={'User-agent': 'Mozilla/5.0'})
-	#if r.status_code == 200:
-		#with open(img_name, 'wb') as f:
-			#r.raw.decode_content = True
-			#shutil.copyfileobj(r.raw, f)
-	##Método3 - dá forbidden
-	#print(requests.cookies.get('HTTP_SESSION'))
-	#opener=urllib.request.build_opener()
-	#opener.addheaders.append(('Cookie','HTTP_SESSION=112087644'))
-	#urllib.request.install_opener(opener)
-	#urllib.request.urlretrieve(img_url,img_name)
-	#Metodo4 - Wget 
-	#wget --save-cookies cookies.txt --keep-session-cookies --auth-no-challenge --user=up201606726 --password=Chitaveloz98 https://sigarra.up.pt/fcup/pt/WEB_PAGE.INICIAL
-	#wget --load-cookies cookies.txt https://sigarra.up.pt/fcup/pt/fotografias_service.foto?pct_cod=201200765
-	break
-
+		#urlretrieve(image, "student"+str(i)+".jpg")
+		#Metodo4 - Wget 
+	
+		#wget --save-cookies cookies.txt --keep-session-cookies --auth-no-challenge --user=up201606726 --password=Chitaveloz98 https://sigarra.up.pt/fcup/pt/WEB_PAGE.INICIAL
+		#wget --load-cookies cookies.txt https://sigarra.up.pt/fcup/pt/fotografias_service.foto?pct_cod=201200765
+	return names
 
 
